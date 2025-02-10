ea421f230a546301cb4a777ddb4e61e2f0fa7251
eduardo-ax <eduardo.axps@gmail.com>
2025-02-10 17:14:31 -0300

    Created cmd/ and internal/ folders. Translate code

---
+++ cmd/api-golang-1/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type User struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+func main() {
+	r := gin.Default()
+
+	r.POST("/user", func(c *gin.Context) {
+		var data User
+
+		if err := c.ShouldBindJSON(&data); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		println("Name:", data.Name)
+		println("Email:", data.Email)
+
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Data received!",
+			"dados":   data,
+		})
+	})
+
+	r.Run(":8080")
+}
