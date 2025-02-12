711904ff8514672df89479bff0f9033ffc4597ec
eduardo-ax <eduardo.axps@gmail.com>
2025-02-12 13:38:04 -0300

    feature: Add custom logger

--- cmd/api-golang-1/main.go
+++ cmd/api-golang-1/main.go
@@ -1,8 +1,22 @@
 package main
 
-import "github.com/eduardo-ax/api-golang-1/router"
+import (
+	config "github.com/eduardo-ax/api-golang-1/config/database"
+	"github.com/eduardo-ax/api-golang-1/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	// Initialize configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
 
 	// Initialize Router
 	router.Initialize()
